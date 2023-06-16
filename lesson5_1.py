2113d3e607fcc08ab7edb1eeaa7a6cc22028b8a9
annadobrianskaia <dobrya-anna@yandex.ru>
2023-06-16 19:32:54 +0300

    task_a

--- lesson5_1.py
+++ lesson5_1.py
@@ -1,4 +1,9 @@
-d ={1: 'eins', 2: 'zwei', 3: 'drei',4: 'vier', 5:'fuenf'}
-for key, value in d.items():
-    print(key, "->", value)
+# d ={1: 'eins', 2: 'zwei', 3: 'drei',4: 'vier', 5:'fuenf'}
+# for key, value in d.items():
+#     print(key, "->", value)
+
+"""sum the values"""
+d = {1:'100', 2:'200', 3:'300'}
+print(sum(d.values()))
+# print(sum(d.values()))
 
