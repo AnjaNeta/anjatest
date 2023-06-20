297dba02c177397fae610e38cdaab9fce048842f
annadobrianskaia <dobrya-anna@yandex.ru>
2023-06-20 20:03:25 +0300

    task_a

--- try.py
+++ try.py
@@ -1,10 +1,19 @@
 # input('enter a number as x ').split()
-degree = input().split()
-temp = input().split()
-def func(degree, temp):
-    if temp == "fahrenheit":
-        degree = (degree * 1.8) +32
-    elif temp == "celcius":
-        degree = (degree -32) /1.8
-    return degree
-print(func(degree,temp))
+# degree = int(input())
+# temp = str(input())
+# def func(degree, temp):
+#     if temp == "fahrenheit":
+#         degree = (degree * 1.8) +32
+#     elif temp == "celcius":
+#         degree = (degree -32) /1.8
+#     return degree
+# print(func(degree,temp))
+
+
+# задача про високосный год
+def year_func(time):
+    if time % 4 ==0 and time % 100 != 0 or time % 400 == 0:
+        return "YES"
+    else:
+        return "NO"
+print(year_func(1600))
