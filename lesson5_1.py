76ce56f3259396b8c5968f2b0152f65946ad2e51
annadobrianskaia <dobrya-anna@yandex.ru>
2023-06-20 21:58:42 +0300

    task_a

--- lesson5_1.py
+++ lesson5_1.py
@@ -3,7 +3,15 @@
 #     print(key, "->", value)
 
 """sum the values"""
-d = {1:'100', 2:'200', 3:'300'}
-print(sum(d.values()))
+# d = {'a': 100, 'b': 200, 'c': 300}
 # print(sum(d.values()))
 
+"""delete the values which are NONE"""
+# d = {'a': 100, 'b': 200, 'c': 300, 'm': 'None', 'e': 500}
+# d ={key:value for key, value in d.items() if value !=None}
+# print(d)
+
+
+names = ["Sofia Goggia", "Mikaela Shiffrin", "Wendy Holdener", "Lindsey Vonn", "Frida Hansdotter", "Michelle Gisin", "Ragnhild Mowinckel", "Federica Brignone", "Tina Weirather", "Ester Ledecka"]
+medals =[3, 10, 7, 11, 6, 2, 3, 2, 2, 6]
+new_list = list(zip["Sofia Goggia", "Mikaela Shiffrin", "Wendy Holdener", "Lindsey Vonn", "Frida Hansdotter", "Michelle Gisin", "Ragnhild Mowinckel", "Federica Brignone", "Tina Weirather", "Ester Ledecka"],())])
---
+++ program.py
@@ -0,0 +1,3 @@
+from package.package_itself import read_input, in_str
+x,y,z =read_input()
+print(x,y,z)
