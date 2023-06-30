dd9c14f7e1173d8669ab5b51c6aba6cda2c73fd8
annadobrianskaia <dobrya-anna@yandex.ru>
2023-06-30 19:17:52 +0300

    lesson 8

---
+++ lesson 9.py
@@ -0,0 +1,6 @@
+def calender(year, month,date):
+    # in_calen = input("enter the date: ").split()
+    # year,month,date = in_calen
+    calender(2012, 2, 15)
+    return print("date is: ".format(date))
+
