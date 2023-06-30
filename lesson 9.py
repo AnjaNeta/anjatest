311fc5537c5b96293a324f6b6f8af42e4cf5ce21
annadobrianskaia <dobrya-anna@yandex.ru>
2023-06-30 19:54:00 +0300

    lesson 8

--- lesson 9.py
+++ lesson 9.py
@@ -1,5 +1,12 @@
-def calender(year, month,date =''):
-    # in_calen = input("enter the date: ").split()
-    # year,month,date = in_calen
-    print("date is {} {} {}".format(year, month,date))
-calender(3,5,2023)
+# def calender(year, month,date =''):
+#     # in_calen = input("enter the date: ").split()
+#     # year,month,date = in_calen
+#     print("date is {} {} {}".format(year, month,date))
+# calender(3,5,2023)
+
+"""принимает неограниченное кол-во насекомых и выводит на печать """
+def insects(*args):
+    print(args)
+insects("butterfly", "fly", "dragonfly")
+
+
