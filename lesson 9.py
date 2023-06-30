ed1853511841465c036121696a635436b9a18cb3
annadobrianskaia <dobrya-anna@yandex.ru>
2023-06-30 20:02:59 +0300

    lesson 8

--- lesson 9.py
+++ lesson 9.py
@@ -4,9 +4,15 @@
 #     print("date is {} {} {}".format(year, month,date))
 # calender(3,5,2023)
 
-"""принимает неограниченное кол-во насекомых и выводит на печать """
-def insects(*args):
-    print(args)
-insects("butterfly", "fly", "dragonfly")
+# """принимает неограниченное кол-во насекомых и выводит на печать """
+# def insects(*args):
+#     print(args)
+# insects("butterfly", "fly", "dragonfly")
+
+"""принимает марка автомобиля и его модель, которая по умолчанию"""
+def salon(car_brand,model ='q7'):
+    print("{} {}".format(car_brand,model))
+salon("hyundai")
+
 
 
