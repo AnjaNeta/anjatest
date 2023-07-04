15a9b2e7aec77dbc07ee2c97ffb4f1e171e95830
annadobrianskaia <dobrya-anna@yandex.ru>
2023-07-04 19:53:42 +0300

    lesson 10

--- lesson 10.py
+++ lesson 10.py
@@ -8,7 +8,8 @@ empl = Employee("Dave", 10000)
 empl.display_employee()
 
 class Freelance(Employee):
-    def __init__(self, email):
+    def __init__(self, name, salary, email):
+        super.__init__(name, salary)
         self.email = email
     def display_email(self):
         print("Email", self.email)
