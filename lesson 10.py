083845120100d2dccd9de924412257030833da2e
annadobrianskaia <dobrya-anna@yandex.ru>
2023-07-04 19:25:27 +0300

    lesson 9_task 5_nenew

---
+++ lesson 10.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self,name, salary):
+        self.name = name
+        self.salary = salary
+    def display_employee(name, salary):
+        return name
+    def display_employee_2(self, salary):
+        return salary
+print("name: , salary: ".split())
