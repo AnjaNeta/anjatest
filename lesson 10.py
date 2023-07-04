5b7bd275814454999f50f63861c3e87b21a7e4bc
annadobrianskaia <dobrya-anna@yandex.ru>
2023-07-04 19:26:00 +0300

    lesson 9_task 5_nenew

--- lesson 10.py
+++ lesson 10.py
@@ -2,7 +2,7 @@ class Employee:
     def __init__(self,name, salary):
         self.name = name
         self.salary = salary
-    def display_employee(name, salary):
+    def display_employee(self,name):
         return name
     def display_employee_2(self, salary):
         return salary
