cd62a778f77631d757e345e6bad0062b37742247
annadobrianskaia <dobrya-anna@yandex.ru>
2023-07-11 19:34:14 +0300

    newnew

--- lesson_11_big_task.py
+++ lesson_11_big_task.py
@@ -4,6 +4,8 @@ class User:
         self.login = login
         self.password = password
         self.mode = mode
+    def __str__(self):
+        return f"login: {self.login}, password: {self.password}, mode :{self.mode}"
 
 list_of_users = []
 
@@ -13,4 +15,6 @@ user_3 = User("taehyungkim",9768427,"user")
 list_of_users.append(user_1)
 list_of_users.append(user_2)
 list_of_users.append(user_3)
-print(list_of_users)
+print(list_of_users[0])
+print(list_of_users[1])
+print(list_of_users[2])
