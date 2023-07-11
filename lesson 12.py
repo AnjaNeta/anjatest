4aface20b10becf7b56b4d12680d9800c90c1a98
annadobrianskaia <dobrya-anna@yandex.ru>
2023-07-11 19:37:23 +0300

    newnew

--- lesson 12.py
+++ lesson 12.py
@@ -7,18 +7,19 @@ class Passport:
         self.number = number
 
     def passport_print(self):
-            print("Name: ", self.name,
-                 "Surname: ", self.surname,
-                  "birthday: ", self.birthday,
-                  "country: ",self.country,
-                  "number: ", self.number)
+            print("Name: ", self.name)
+            print("Surname: ", self.surname)
+            print("birthday: ", self.birthday)
+            print("country: ",self.country)
+            print("number: ", self.number)
 detail = Passport ("Anna", "Dobrian", 11071994, "Russian Federation", 2345796858)
 detail.passport_print()
-class Foreignpassport(Passport):
+class ForeignPassport(Passport):
     def  __init__(self, name, surname, birthday, country, number, visa):
+        super().passport_print
         super().__init__(name, surname, birthday, country, number)
         self.visa = visa
     def visaprint(self):
         print("visa", self.visa)
-details = Foreignpassport("Anna", "Dobrian", 11071994, "Russian Federation", 2345796858, "Germany")
-details.visaprint()
+details = ForeignPassport("Anna", "Dobrian", 11071994, "Russian Federation", 2345796858, "Germany")
+details.passport_print()
